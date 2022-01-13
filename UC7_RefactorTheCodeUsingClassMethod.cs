3eb1f8720cb3f1b200d1d21cd22743a10120ed7e
Sukalyan8 <96485897+Sukalyan8@users.noreply.github.com>
2022-01-13 15:06:17 +0530

    Update UC7_RefactorTheCodeUsingClassMethod.cs

--- UC7_RefactorTheCodeUsingClassMethod.cs
+++ UC7_RefactorTheCodeUsingClassMethod.cs
@@ -43,7 +43,7 @@ namespace EmployeeWageComputationProblem
             return totalEmpWage;
         }
 
-        public void RefactotTheCode() 
+        public void RefactorTheCode() 
         {
           ComputeEmpWage();
         }
